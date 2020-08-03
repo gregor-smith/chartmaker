d19598e9c6b9c643cec3f03d65c65f0690dccf4e
Gregor Smith <gregor_smith@outlook.com>
2020-08-03 17:23:36 +0100

    controlled form, input tests

--- tests/components/ControlledForm.test.tsx
+++ tests/components/ControlledForm.test.tsx
@@ -1,4 +1,41 @@
-test.todo('renders form')
+import React from 'react'
+import { render } from 'react-dom'
+import { act } from 'react-dom/test-utils'
+
+import { ControlledForm } from '@/components/ControlledForm'
+
+import { RenderContainer, ignore, fireEvent } from '../utils'
 
 
-test.todo('submit calls onSubmit prop')
+const container = new RenderContainer()
+
+
+test('renders form', () => {
+    render(
+        <ControlledForm onSubmit={ignore}>
+            test form
+        </ControlledForm>,
+        container.element
+    )
+
+    expect(container.element).toMatchSnapshot()
+})
+
+
+test('submit calls onSubmit prop', () => {
+    const mock = jest.fn<void, []>()
+
+    render(
+        <ControlledForm onSubmit={mock}/>,
+        container.element
+    )
+
+    act(() =>
+        fireEvent(
+            'submit',
+            container.element?.querySelector('form')
+        )
+    )
+
+    expect(mock).toHaveBeenCalledTimes(1)
+})
--- tests/components/ControlledInput.test.tsx
+++ tests/components/ControlledInput.test.tsx
@@ -1,6 +1,83 @@
-test.each([ 'text', 'password' ] as const)('renders input', _type => {
+import React from 'react'
+import { render } from 'react-dom'
+import { act } from 'react-dom/test-utils'
 
+import { ControlledInput } from '@/components/ControlledInput'
+
+import { RenderContainer, ignore, fireEvent } from '../utils'
+
+
+const container = new RenderContainer()
+
+
+test.each([ 'text', 'password', undefined ] as const)('renders text type input', type => {
+    render(
+        <ControlledInput type={type}
+            id='test-input'
+            value='test value'
+            onChange={ignore}/>,
+        container.element
+    )
+
+    expect(container.element).toMatchSnapshot()
 })
 
 
-test.todo('change event calls onChange prop')
+test.each([ true, false, undefined ])('renders disabled prop', disabled => {
+    render(
+        <ControlledInput id='test-input'
+            value='test value'
+            disabled={disabled}
+            onChange={ignore}/>,
+        container.element
+    )
+
+    expect(container.element).toMatchSnapshot()
+})
+
+
+test.each([ 'test-1', 'test-2' ])('renders id prop', id => {
+    render(
+        <ControlledInput id={id}
+            value='test value'
+            onChange={ignore}/>,
+        container.element
+    )
+
+    expect(container.element).toMatchSnapshot()
+})
+
+
+test.each([ 'test-1', 'test-2' ])('renders value prop', value => {
+    render(
+        <ControlledInput id='test-input'
+            value={value}
+            onChange={ignore}/>,
+        container.element
+    )
+
+    expect(container.element).toMatchSnapshot()
+})
+
+
+test('change event calls onChange prop', () => {
+    const mock = jest.fn<void, [ string ]>()
+
+    render(
+        <ControlledInput id='test-input'
+            value='test value'
+            onChange={mock}/>,
+        container.element
+    )
+
+    act(() =>
+        fireEvent(
+            'change',
+            container.element?.querySelector('input'),
+            { target: { value: 'new value' } }
+        )
+    )
+
+    expect(mock).toHaveBeenCalledTimes(1)
+    expect(mock).toHaveBeenCalledWith('new value')
+})
