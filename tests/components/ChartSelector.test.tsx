13113b313aae856c4a37a3e4bb0f433b1335a1b1
Gregor Smith <gregor_smith@outlook.com>
2020-08-03 15:06:33 +0100

    chartselector tests

--- tests/components/ChartSelector.test.tsx
+++ tests/components/ChartSelector.test.tsx
@@ -1,7 +1,82 @@
-test.todo('renders labelled select')
+import React from 'react'
+import { render } from 'react-dom'
+import { act } from 'react-dom/test-utils'
+
+import { Chart } from '@/types'
+import { ChartSelector, ChartSelectorProps } from '@/components/ChartSelector'
+
+import {
+    RenderContainer,
+    ignore,
+    fireEvent
+} from '../utils'
 
 
-test.todo('activeChartIndex prop changes selected chart')
+const container = new RenderContainer()
+
+const charts: Chart[] = [
+    {
+        name: 'test chart 1',
+        rowsX: 5,
+        rowsY: 5,
+        shape: { tag: 'Collage' },
+        // this component doesn't touch the albums at all so no need to
+        // actually pass any
+        albums: []
+    },
+    {
+        name: 'test chart 2',
+        rowsX: 10,
+        rowsY: 10,
+        shape: { tag: 'Top', size: 100 },
+        albums: []
+    },
+    {
+        name: 'test chart 3',
+        rowsX: 10,
+        rowsY: 10,
+        shape: { tag: 'Top', size: 42 },
+        albums: []
+    }
+]
 
 
-test.todo('changing selected chart dispatches action')
+type ActionParams = Parameters<ChartSelectorProps['dispatch']>;
+
+
+test('renders labelled select', () => {
+    render(
+        <ChartSelector dispatch={ignore}
+            charts={charts}
+            activeChartIndex={1}/>,
+        container.element
+    )
+
+    expect(container.element).toMatchSnapshot()
+})
+
+
+test('changing selected chart dispatches action', () => {
+    const mock = jest.fn<void, ActionParams>()
+
+    render(
+        <ChartSelector dispatch={mock}
+            charts={charts}
+            activeChartIndex={0}/>,
+        container.element
+    )
+
+    act(() =>
+        fireEvent(
+            'change',
+            container.element?.querySelector('select'),
+            { target: { value: '1' } }
+        )
+    )
+
+    expect(mock).toHaveBeenCalledTimes(1)
+    expect(mock).toHaveBeenCalledWith<ActionParams>({
+        tag: 'UpdateActiveChart',
+        index: 1
+    })
+})
