dba94e8f39693bb0ade8f0bfaca24fb63dbb6bfb
Gregor Smith <gregor_smith@outlook.com>
2021-02-05 01:33:07 +0000

    red error text on invalid viewer url

--- src/pages/Viewer.tsx
+++ src/pages/Viewer.tsx
@@ -1,9 +1,16 @@
+import { css } from 'emotion'
 import type { FC, RefObject } from 'react'
 
 import type { DispatchProps } from '@/reducer'
 import type { Chart, ScreenshotState } from '@/types'
 import { ViewerSidebar } from '@/components/ViewerSidebar'
 import { ViewerChart } from '@/components/ViewerChart'
+import { ERROR_TEXT_COLOUR } from '@/style'
+
+
+const errorStyle = css({
+    color: ERROR_TEXT_COLOUR
+})
 
 
 export type ViewerProps =
@@ -25,7 +32,13 @@ export const Viewer: FC<ViewerProps> = ({
 }) => {
     let content: JSX.Element
     if (chart === null) {
-        content = <main>Invalid chart URL</main>
+        content = (
+            <main>
+                <h1 className={errorStyle}>
+                    Invalid chart URL
+                </h1>
+            </main>
+        )
     }
     else {
         content = (
